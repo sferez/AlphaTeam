18e05f217f8ee8ecbc32e5bd038d6af3716a69e8
sferez <simeon.ferez@gmail.com>
2023-05-03 18:19:39 +0100

    feat: update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'alabaster'
+# html_theme = 'sphinx_book_theme'
 html_logo = '_static/favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
